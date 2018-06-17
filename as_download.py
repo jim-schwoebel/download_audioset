0258a6a045bca2a332623cb9f53907669e92bf53
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-06-17 16:31:39 -0400

    Update as_download.py

--- as_download.py
+++ as_download.py
@@ -4,7 +4,7 @@
 '''
 Script name: download_audioset.py
 Author: Jim Schwoebel
-License: BSD 3.0
+License: Apache 2.0
 
 This script parses through the entire balanced audioset dataset and downloads
 all the raw audio files. The files are arranged in folders according to their
@@ -17,6 +17,25 @@ all the files.
 Enjoy! - :) 
 
 -Jim 
+'''
+
+################################################################################
+##                        LICENSE INFORMATION                                 ##       
+################################################################################
+'''
+Copyright 2018 Jim Schwoebel
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 '''
 
 ################################################################################
