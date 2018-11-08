631be596024aa8881289e3e45811e800604850e6
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-11-08 14:59:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+'''
+Quick setup script.
+'''
 import os 
 
+def pip_install(modules):
+    for i in range(len(modules)):
+        os.system('pip3 install %s'%(modules[i]))
+
 os.system('brew install ffmpeg')
-os.system('pip3 install ffmpy')
-os.system('pip3 install pandas')
-os.system('pip3 install soundfile')
-os.system('pip3 install pafy')
+modules=['ffmpy','pandas','soundfile','pafy']
+pip_install(modules)
